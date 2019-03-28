499e1931af3569ae29d890a2cae420f407822100
Robert Hsu <robert@easypost.com>
2019-03-28 15:40:56 -0700

    added in init dealer

--- services/dealer_service.py
+++ services/dealer_service.py
@@ -1,6 +1,15 @@
 class DealerService:
-    def __init__(self, dealer):
-        self.dealer = dealer
+    def __init__(self, user_data):
+        self.user_data = user_data
 
-    def play(self):
-        
+    def dealer_hand(self):
+        return self.user_data["dealer_hand"]
+
+    def deck(self):
+        return self.user_data["deck"]
+
+    def init_dealer(self):
+        if (len(self.dealer_hand()) != 0):
+            raise RuntimeError("dealer is already initialized")
+        self.dealer_hand().append(self.deck().pop())
+        self.dealer_hand().append(self.deck().pop())
