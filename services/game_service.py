80945b83a4fa5d525bdeb0c9baae3d3ebf5694b2
Robert Hsu <robert@easypost.com>
2019-03-28 14:21:43 -0700

    backup

--- services/game_service.py
+++ services/game_service.py
@@ -1,21 +1,25 @@
 class GameService:
     def __init__(self, user_data):
         self.user_data = user_data
-        self.deck = user_data["deck"]
-        self.hand = user_data["hand"]
+
+    def deck(self):
+        return self.user_data["deck"]
+
+    def hand(self):
+        return self.user_data["hand"]
 
     def play(self):
-        if len(self.hand) == 0:
-            self.hand.append(self.deck.deal())
-            self.hand.append(self.deck.deal())
+        if len(self.hand()) == 0:
+            self.hand().append(self.deck().deal())
+            self.hand().append(self.deck().deal())
             return self._hand_to_string()
         else:
-            self.hand.append(self.deck.deal())
+            self.hand().append(self.deck().deal())
             total_value = 0
-            for card in self.hand:
+            for card in self.hand():
                 total_value += card.value()
             if total_value == 21:
-                return "Black Jack You Win % s" % (self._hand_to_string())
+                return "21: You Win % s" % (self._hand_to_string())
             elif total_value > 21:
                 hand_string = self._hand_to_string()
                 self.user_data["hand"] = []
@@ -24,4 +28,4 @@ class GameService:
                 return "Your hand is %s" % (self._hand_to_string())
 
     def _hand_to_string(self):
-        return " ".join(map(lambda x: str(x), self.hand))
+        return " ".join(map(lambda x: str(x), self.hand()))
