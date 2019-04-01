3c9d836e506a6d97ab7b013cc1001aca30e296d2
Robert Hsu <robert@easypost.com>
2019-04-01 15:48:16 -0700

    another test

--- test/services_tests/test_endgame_service.py
+++ test/services_tests/test_endgame_service.py
@@ -12,15 +12,22 @@ def test_dealer_busted(
                      "someone wins!"
 
 
-# def test_dealer_wins(endgame_service_fixture):
-#     # EndgameService(fake_user_id)
-#     pass
+def test_dealer_wins(
+    endgame_service_fixture, default_user_data, losing_hand, winning_hand
+):
+    default_user_data["dealer_hand"] = winning_hand
+    default_user_data["hand"] = losing_hand
+    result = endgame_service_fixture.determine()
+    assert result == "Dealer has: 9 :clubs: 2 :clubs: 10 :clubs:. someone "\
+                     "has: 2 :hearts: 2 :spades. someone loses!"
 
 
 # def test_dealer_loses(endgame_service_fixture):
 #     # EndgameService(fake_user_id)
+#     from pdb import set_trace; set_trace()
 #     pass
 
 
 # def test_cant_stand():
+#     from pdb import set_trace; set_trace()
 #     pass
