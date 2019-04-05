9a1a2cce2f368d2fc97eb1015e9c08cf67ecc6c5
Robert Hsu <robert@easypost.com>
2019-04-05 11:36:17 -0700

    wrote service

--- services/service.py
+++ services/service.py
@@ -1,3 +1,21 @@
 class Service:
-    def __init__():
-        pass
+    def __init__(self, userdata):
+        self.userdata = userdata
+
+    def username(self):
+        return self.userdata["username"]
+
+    def hand(self):
+        return self.userdata["hand"]
+
+    def deck(self):
+        return self.userdata["deck"]
+
+    def dealer_hand(self):
+        return self.userdata["dealer_hand"]
+
+    def bet(self):
+        return self.userdata["bet"]
+
+    def money(self):
+        return self.userdata["money"]
