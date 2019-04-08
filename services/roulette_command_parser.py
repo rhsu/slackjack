def10fc217e44052d5a818374361e60cac8bb088
Robert Hsu <robert@easypost.com>
2019-04-08 13:51:33 -0700

    backup

--- services/roulette_command_parser.py
+++ services/roulette_command_parser.py
@@ -1,3 +1,35 @@
 class RouletteCommandParser:
-    def __init__(self):
-        pass
+    def __init__(self, command):
+        self.command = command
+
+    def is_valid(self):
+        tokens = self.command.split(" ")
+
+        # look at all the tokens
+        if len(tokens) != 4:
+            return False, "Invalid *put* command"
+
+        # look at token 1
+        bet_amount = tokens[1]
+        try:
+            bet_amount = int(tokens[1])
+        except ValueError:
+            return False, "Invalid bet amount"
+            if bet_amount <= 0:
+                return False, "Invalid bet amount"
+
+        # look at token 2
+        if tokens[2].lower() != "on":
+            return False, "Invalid *put* command: missing *on* keyword"
+
+        # look at token 3
+        if tokens[3].lower() == "red" or tokens[3].lower == "black":
+            return True
+
+        bet_number = tokens[3]
+        try:
+            bet_number = int(tokens[3])
+        except ValueError:
+            return False, "Invalid bet number"
+            if bet_number <= 0:
+                return False, "Invalid bet number"
