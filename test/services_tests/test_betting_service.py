c047587975cce49db83d16acf991a8bafb4629d7
Robert Hsu <robert@easypost.com>
2019-04-08 13:01:29 -0700

    bakcvup

--- test/services_tests/test_betting_service.py
+++ test/services_tests/test_betting_service.py
@@ -4,7 +4,7 @@ from test.mocks.mock_objects import MockGameService
 
 def test_place_bet_when_no_money(default_user_data):
     game_service = MockGameService()
-    default_user_data["money"] = 0
+    default_user_data.money = 0
     service = BettingService(default_user_data, game_service)
     result = service.place_bet(5)
     assert result == "someone: Can't play. Not enough money."
@@ -26,7 +26,7 @@ def test_place_bet_when_bet_exists(default_user_data):
 
 def test_place_bet_when_successful(default_user_data):
     game_service = MockGameService()
-    default_user_data["bet"] = 0
+    default_user_data.bet = 0
     service = BettingService(default_user_data, game_service)
     result = service.place_bet(1)
     assert result == "someone has bet 1. MOCK"
