95ee2168aee75169d39fca23c8fc3b1716b82473
rhsu <rhsu123@gmail.com>
2019-04-09 16:39:32 -0700

    ace hotfix (#43)
    * ace hotfix
    
    * remove a comment

--- utils/hand_util.py
+++ utils/hand_util.py
@@ -6,7 +6,7 @@ def hand_sum(hand):
     hand_copy = list(hand)
     simple_sum = _simple_sum(hand_copy)
     if _contains_ace(hand_copy):
-        new_sum = 11 + _simple_sum(hand)
+        new_sum = 11 + _simple_sum(hand_copy)
         if new_sum > 21:
             return simple_sum
         else:
