d24dab1b8be1ba6a19e0ba0966c4790f58e072db
Robert Hsu <robert@easypost.com>
2019-04-12 15:49:56 -0700

    backup

--- test/services_tests/test_roulette_command_parser.py
+++ test/services_tests/test_roulette_command_parser.py
@@ -1,17 +1,22 @@
 from services.roulette_command_parser import RouletteCommandParser
 
 
-def test_valid_betting_on_black():
+def test_valid_betting_on_black(default_user_data, default_id):
     command = "put 50 on black"
-    response = RouletteCommandParser(command).is_valid()
+    response = RouletteCommandParser(
+        command, default_id, default_user_data).is_valid()
     assert response == (True, "success")
 
 
-def test_valid_betting_on_red():
+def test_valid_betting_on_red(default_user_data, default_id):
     command = "put 50 on red"
-    assert RouletteCommandParser(command).is_valid()
+    response = RouletteCommandParser(
+        command, default_id, default_user_data).is_valid()
+    assert response == (True, "success")
 
 
-def test_valid_betting_on_number():
+def test_valid_betting_on_number(default_user_data, default_id):
     command = "put 50 on 5"
-    assert RouletteCommandParser(command).is_valid()
+    response = RouletteCommandParser(
+        command, default_id, default_user_data).is_valid()
+    assert response == (True, "success")
