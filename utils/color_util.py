652deb71c1cf6d39f793b31d5dca1966674e6028
Robert Hsu <robert@easypost.com>
2019-04-24 11:49:08 -0700

    backup

--- utils/color_util.py
+++ utils/color_util.py
@@ -1,13 +1,9 @@
-# In number ranges from 1 to 10 and 19 to 28,
-# # odd numbers are red and even are black.
-#  In ranges from 11 to 18 and 29 to 36,
-# # odd numbers are black and even are red.
-
-
 def determine_color(number):
-    if number == "00" or number == "0":
-        return "green"
-    elif number >= 1 and number <= 10:
+    """
+    In number ranges from 1 to 10 and 19 to 28, odd numbers are red and even are black.
+    In ranges from 11 to 18 and 29 to 36, odd numbers are black and even are red.
+    """
+    if number >= 1 and number <= 10:
         return "black" if number % 2 == 0 else "red"
     elif number <= 19 and number <= 28:
         return "black" if number % 2 == 0 else "red"
