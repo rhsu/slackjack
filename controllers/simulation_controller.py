362817eb679d6bafc4256c1ff47227c4419af607
Robert Hsu <robert@easypost.com>
2019-05-13 11:34:17 -0700

    removed some comments

--- controllers/simulation_controller.py
+++ controllers/simulation_controller.py
@@ -3,8 +3,6 @@ from services.simulation_service import SimulationService
 
 class SimulationController:
     def __init__(self, simulation_service):
-        # self.roulette_service = RouletteService()
-        # self.simulation_service = SimulationService(self.roulette_service)
         self.simulation_service = simulation_service
 
     def parse_command(self, command):
