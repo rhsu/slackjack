7857e6dfbfe4ceb2237a7489d06459c384805630
Robert Hsu <robert@easypost.com>
2019-05-14 16:21:46 -0700

    backup

--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -9,6 +9,7 @@ from services.register_service import RegisterService
 from services.roulette_command_service import RouletteCommandService
 from services.roulette_service import RouletteService
 from services.help_service import HelpService
+from services.roulette_queue_manager_service import RouletteQueueManangerService
 
 
 class GameController:
@@ -21,12 +22,10 @@ class GameController:
             self.user_data = GLOBAL_STORE[user_id]
             self.rebuy_service = RebuyService(self.user_data)
             self.dealer_service = DealerService(self.user_data)
-            self.endgame_service = EndgameService(
-                self.user_data, self.dealer_service)
-            self.game_service = GameService(
-                self.user_data, self.endgame_service)
-            self.betting_service = BettingService(
-                self.user_data, self.game_service)
+            self.endgame_service = EndgameService(self.user_data, self.dealer_service)
+            self.game_service = GameService(self.user_data, self.endgame_service)
+            self.betting_service = BettingService(self.user_data, self.game_service)
+            self.roulette_queue_manager_service = RouletteQueueManangerService(self.roulette_service)
 
     def parse_command(self, command):
         command = command.lower()
@@ -78,29 +77,29 @@ class GameController:
                 return "error: %s" % (parsed[1])
         elif command.startswith("start"):
             result, color = self.roulette_service.spin()
-            ret_val = f"the result is *{result}* (*{color}*) \n"
-            for user_id in ROULETE_QUEUE:
-                curr_user = GLOBAL_STORE[user_id]
-                if curr_user.roulette_bet == color:
-                    curr_user.money += curr_user.roulette_bet_amount
-                    ret_val += "*%s* bet on *%s*. *%s* won \n" % (
-                        curr_user.username,
-                        curr_user.roulette_bet,
-                        curr_user.username)
-                elif curr_user.roulette_bet == result:
-                    curr_user.money += curr_user.roulette_bet_amount
-                    ret_val += "*%s* bet on *%s*. *%s* won. \n" % (
-                        curr_user.username,
-                        curr_user.roulette_bet,
-                        curr_user.username)
-                else:
-                    curr_user.money -= curr_user.roulette_bet_amount
-                    ret_val += "*%s* bet on *%s*. *%s* lost. \n" % (
-                        curr_user.username,
-                        curr_user.roulette_bet,
-                        curr_user.username)
-            del ROULETE_QUEUE[:]
-            return ret_val
+            # ret_val = f"the result is *{result}* (*{color}*) \n"
+            # for user_id in ROULETE_QUEUE:
+            #     curr_user = GLOBAL_STORE[user_id]
+            #     if curr_user.roulette_bet == color:
+            #         curr_user.money += curr_user.roulette_bet_amount
+            #         ret_val += "*%s* bet on *%s*. *%s* won \n" % (
+            #             curr_user.username,
+            #             curr_user.roulette_bet,
+            #             curr_user.username)
+            #     elif curr_user.roulette_bet == result:
+            #         curr_user.money += curr_user.roulette_bet_amount
+            #         ret_val += "*%s* bet on *%s*. *%s* won. \n" % (
+            #             curr_user.username,
+            #             curr_user.roulette_bet,
+            #             curr_user.username)
+            #     else:
+            #         curr_user.money -= curr_user.roulette_bet_amount
+            #         ret_val += "*%s* bet on *%s*. *%s* lost. \n" % (
+            #             curr_user.username,
+            #             curr_user.roulette_bet,
+            #             curr_user.username)
+            # del ROULETE_QUEUE[:]
+            # return ret_val
         elif command.startswith("rebuy"):
             return self.rebuy_service.rebuy()
         elif command.startswith("help"):
--- services/roulette_queue_manager_service.py
+++ services/roulette_queue_manager_service.py
@@ -1,51 +1,32 @@
 class RouletteQueueManangerService():
-    def __init__(self, color, global_store, number, roulette_queue):
-        self.color = color
+    def __init__(self, global_store, roulette_queue):
         self.global_store = global_store
-        self.number = number
         self.roulette_queue = roulette_queue
 
-    def determine(self):
+    def determine(self, color, number):
+        ret_val = ""
         for user_id in self.roulette_queue:
             curr_user = self.global_store[user_id]
-
-
-    # def __init__(self, global_store, roulette_queue, number, color):
-    #     self.global_store = global_store
-    #     self.roulette_queue = roulette_queue
-    #     self.number = number
-    #     self.color = color
-
-    # def determine(self):
-    #     for user_id in self.roulette_queue:
-    #         bet = self.global_store[user_id]["bet"]
-    #         if bet == self.color:
-    #             pass
-    #         elif bet == self.result:
-    #             pass
-    #         else:
-    #             pass
-
-# result, color = self.roulette_service.spin()
-#             ret_val = f"the result is *{result}* (*{color}*) \n"
-#             for user_id in ROULETE_QUEUE:
-#                 curr_user = GLOBAL_STORE[user_id]
-#                 if curr_user.roulette_bet == color:
-#                     curr_user.money += curr_user.roulette_bet_amount
-#                     ret_val += "*%s* bet on *%s*. *%s* won \n" % (
-#                         curr_user.username,
-#                         curr_user.roulette_bet,
-#                         curr_user.username)
-#                 elif curr_user.roulette_bet == result:
-#                     curr_user.money += curr_user.roulette_bet_amount
-#                     ret_val += "*%s* bet on *%s*. *%s* won. \n" % (
-#                         curr_user.username,
-#                         curr_user.roulette_bet,
-#                         curr_user.username)
-#                 else:
-#                     curr_user.money -= curr_user.roulette_bet_amount
-#                     ret_val += "*%s* bet on *%s*. *%s* lost. \n" % (
-#                         curr_user.username,
-#                         curr_user.roulette_bet,
-#                         curr_user.username)
-#             del ROULETE_QUEUE[:]
+            if curr_user.roulette_bet == self.color:
+                curr_user.money += curr_user.roulette_bet_amount
+                ret_val += "*%s* bet on *%s*. *%s* won \n" % (
+                    curr_user.username,
+                    curr_user.roulette_bet,
+                    curr_user.username
+                )
+            elif curr_user.roulette_bet == self.result:
+                curr_user.money += curr_user.roulette_bet_amount
+                ret_val += "*%s* bet on *%s*. *%s* won. \n" % (
+                    curr_user.username,
+                    curr_user.roulette_bet,
+                    curr_user.username
+                )
+            else:
+                curr_user.money -= curr_user.roulette_bet_amount
+                ret_val += "*%s* bet on *%s*. *%s* lost. \n" % (
+                    curr_user.username,
+                    curr_user.roulette_bet,
+                    curr_user.username
+                )
+        del self.roulette_queue[:]
+        return ret_val
