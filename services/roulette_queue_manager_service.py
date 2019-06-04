afae3634ce4631db0fdf55a9a33791728c61a24c
Robert Hsu <robert@easypost.com>
2019-06-04 12:34:08 -0700

    Revert "some progress"
    This reverts commit fd5729b1741f1abd9a27add80fb5871408890ea8.

--- services/roulette_queue_manager_service.py
+++ services/roulette_queue_manager_service.py
@@ -10,50 +10,26 @@ class RouletteQueueManangerService():
         ret_val = [f"The result is {number} {determine_icon(color)}. \n"]
         for user_id in self.roulette_queue:
             curr_user = self.global_store[user_id]
-            ret_val += self.determine_if_user_won(curr_user, number, color)
-            # if curr_user.roulette_bet == color:
-            #     curr_user.money += curr_user.roulette_bet_amount
-            #     ret_val.append("*%s* bet on *%s*. *%s* won. \n" % (
-            #         curr_user.username,
-            #         determine_icon(curr_user.roulette_bet),
-            #         curr_user.username
-            #     ))
-            # elif curr_user.roulette_bet == number:
-            #     curr_user.money += curr_user.roulette_bet_amount
-            #     ret_val.append("*%s* bet on *%s*. *%s* won. \n" % (
-            #         curr_user.username,
-            #         determine_icon(curr_user.roulette_bet),
-            #         curr_user.username
-            #     ))
-            # else:
-            #     curr_user.money -= curr_user.roulette_bet_amount
-            #     ret_val.append("*%s* bet on *%s*. *%s* lost. \n" % (
-            #         curr_user.username,
-            #         determine_icon(curr_user.roulette_bet),
-            #         curr_user.username
-            #     ))
-        del self.roulette_queue[:]
-        return " ".join(ret_val)
-
-    def determine_if_user_won(self, curr_user, number, color):
-        # from pdb import set_trace; set_trace()
-        ret_val = []
-        for bet, bet_amount in curr_user.roulette_bet_v2:
-            # from pdb import set_trace; set_trace()
-            if (bet == number or bet == color):
-                curr_user.money += bet_amount
-                # TODO clear roulette_bet_v2
+            if curr_user.roulette_bet == color:
+                curr_user.money += curr_user.roulette_bet_amount
                 ret_val.append("*%s* bet on *%s*. *%s* won. \n" % (
                     curr_user.username,
-                    determine_icon(bet),
+                    determine_icon(curr_user.roulette_bet),
+                    curr_user.username
+                ))
+            elif curr_user.roulette_bet == number:
+                curr_user.money += curr_user.roulette_bet_amount
+                ret_val.append("*%s* bet on *%s*. *%s* won. \n" % (
+                    curr_user.username,
+                    determine_icon(curr_user.roulette_bet),
                     curr_user.username
                 ))
             else:
-                curr_user.money -= bet_amount
-                # TODO clear roulette_bet_v2
+                curr_user.money -= curr_user.roulette_bet_amount
                 ret_val.append("*%s* bet on *%s*. *%s* lost. \n" % (
                     curr_user.username,
                     determine_icon(curr_user.roulette_bet),
                     curr_user.username
                 ))
-        return ret_val
+        del self.roulette_queue[:]
+        return " ".join(ret_val)
