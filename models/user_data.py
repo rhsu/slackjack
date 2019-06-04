b6667a4f4f1cfb2961c86111d571970ed1ce400e
Robert Hsu <robert@easypost.com>
2019-06-04 12:34:54 -0700

    Revert "Revert "some progress""
    This reverts commit afae3634ce4631db0fdf55a9a33791728c61a24c.

--- models/user_data.py
+++ models/user_data.py
@@ -12,7 +12,7 @@ class UserData:
         self.roulette_bet = None
         self.roulette_bet_amount = 0
 
-        self.roulette_bet_v2 = {}
+        self.roulette_bet_v2 = []
 
     def reset_hands(self):
         self.hand = []
