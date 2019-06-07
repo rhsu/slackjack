5c8727c46356d243faf9b15edd37dadef8eac75a
Robert Hsu <robert@easypost.com>
2019-06-07 12:41:10 -0700

    deleted old field

--- models/user_data.py
+++ models/user_data.py
@@ -9,9 +9,6 @@ class UserData:
         self.hand = []
         self.money = 100
         self.username = username
-        self.roulette_bet = None
-        self.roulette_bet_amount = 0
-
         self.roulette_bet_v2 = []
 
     def reset_hands(self):
