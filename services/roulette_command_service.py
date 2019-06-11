ae500bddc69d5c399b8a2d1d8d9dcf15ecb577d0
Robert Hsu <robert@easypost.com>
2019-06-11 10:54:51 -0700

    backup

--- services/roulette_command_service.py
+++ services/roulette_command_service.py
@@ -9,7 +9,7 @@ class RouletteCommandService:
         self.user_id = user_id
         self.user_data = user_data
         self.bet_amount = None
-        self.bet_number = None
+        self.bet_on = None
 
     def is_valid(self):
         # look for all the tokens
@@ -32,7 +32,7 @@ class RouletteCommandService:
                 return False, f"{color_error} or {number_error}"
 
         ROULETE_QUEUE.add(self.user_id)
-        self.user_data.roulette_bet.append((self.bet_number, self.bet_amount))
+        self.user_data.roulette_bet.append((self.bet_on, self.bet_amount))
         return True, "success"
 
     def __validation_token_1(self):
@@ -68,20 +68,20 @@ class RouletteCommandService:
 
         if bet_token in valid_color_bets:
             if bet_token in icon_color_mapping:
-                self.bet_number = icon_color_mapping["bet_token"]
+                self.bet_on = icon_color_mapping["bet_token"]
             else:
-                self.bet_number = bet_token
+                self.bet_on = bet_token
             return True, None
         else:
             return False, "Invalid Bet Color"
 
     def __valid_number_bet(self):
         try:
-            self.bet_number = int(self.tokens[3])
+            self.bet_on = int(self.tokens[3])
         except ValueError:
             return False, "Invalid Bet Number"
 
-        if self.bet_number <= 0 or self.bet_number > 28:
+        if self.bet_on <= 0 or self.bet_on > 28:
             return False, "Invalid Bet Number"
 
         return True, None
