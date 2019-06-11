08906a7a9478e4848dd224ab2860ff2df26afff5
rhsu <rhsu123@gmail.com>
2019-06-11 10:55:15 -0700

    refactor roulette command service (#60)

--- services/roulette_command_service.py
+++ services/roulette_command_service.py
@@ -5,59 +5,83 @@ from utils.color_util import ICONS
 class RouletteCommandService:
     # TODO put user_id on user_data
     def __init__(self, command, user_id, user_data):
-        self.command = command
+        self.tokens = command.split(" ")
         self.user_id = user_id
         self.user_data = user_data
+        self.bet_amount = None
+        self.bet_on = None
 
     def is_valid(self):
-        tokens = self.command.split(" ")
-
-        # look at all the tokens
-        if len(tokens) != 4:
+        # look for all the tokens
+        if len(self.tokens) != 4:
             return False, "Invalid *put* command"
 
-        # look at token 1
-        bet_amount = tokens[1]
+        success_1, error_1 = self.__validation_token_1()
+        if not success_1:
+            return False, error_1
 
-        try:
-            bet_amount = int(tokens[1])
-        except ValueError:
-            return False, "Invalid bet amount"
+        success_2, error_2 = self.__validation_token_2()
+        if not success_2:
+            return False, error_2
 
-        if bet_amount <= 0:
-            return False, "Invalid bet amount"
-
-        if bet_amount > self.user_data.money:
-            return False, "Invalid bet amount: not enough money"
-
-        # look at token 2
-        if tokens[2].lower() != "on":
-            return False, "Invalid *put* command: missing *on* keyword"
-
-        # look at token 3
-        valid_color_bets = set([
-            "red", "black", "green", ICONS["red"], ICONS["black"], ICONS["green"]])
-        if tokens[3].lower() in valid_color_bets:
-            ROULETE_QUEUE.add(self.user_id)
-            if tokens[3] == ICONS["red"]:
-                self.user_data.roulette_bet.append(("red", bet_amount))
-            elif tokens[3] == ICONS["black"]:
-                self.user_data.roulette_bet.append(("black", bet_amount))
-            elif tokens[3] == ICONS["green"]:
-                self.user_data.roulette_bet.append(("green", bet_amount))
-            else:
-                self.user_data.roulette_bet.append((tokens[3], bet_amount))
-            return True, "success"
-
-        bet_number = tokens[3]
-        try:
-            bet_number = int(tokens[3])
-        except ValueError:
-            return False, "Invalid bet type: Cant bet on %s" % bet_number
-
-        if bet_number <= 0 or bet_number > 28:
-            return False, "Invalid bet number"
+        # Validation Token 3 here
+        valid_color, color_error = self.__valid_color_bet()
+        if not valid_color:
+            valid_number, number_error = self.__valid_number_bet()
+            if not valid_number:
+                return False, f"{color_error} or {number_error}"
 
         ROULETE_QUEUE.add(self.user_id)
-        self.user_data.roulette_bet.append((bet_number, bet_amount))
+        self.user_data.roulette_bet.append((self.bet_on, self.bet_amount))
         return True, "success"
+
+    def __validation_token_1(self):
+        try:
+            self.bet_amount = int(self.tokens[1])
+        except ValueError:
+            return False, "Invalid bet amount"
+
+        if self.bet_amount <= 0:
+            return False, "Invalid bet amount"
+
+        if self.bet_amount > self.user_data.money:
+            return False, "Invalid bet amount: not enough money"
+        return True, None
+
+    def __validation_token_2(self):
+        if self.tokens[2].lower() != "on":
+            return False, "Invalid *put* command: missing *on* keyword"
+        return True, None
+
+    def __valid_color_bet(self):
+        valid_color_bets = set([
+            "red", "black", "green", ICONS["red"], ICONS["black"], ICONS["green"]
+        ])
+
+        icon_color_mapping = {
+            ICONS["red"]: "red",
+            ICONS["black"]: "black",
+            ICONS["green"]: "green"
+        }
+
+        bet_token = self.tokens[3]
+
+        if bet_token in valid_color_bets:
+            if bet_token in icon_color_mapping:
+                self.bet_on = icon_color_mapping["bet_token"]
+            else:
+                self.bet_on = bet_token
+            return True, None
+        else:
+            return False, "Invalid Bet Color"
+
+    def __valid_number_bet(self):
+        try:
+            self.bet_on = int(self.tokens[3])
+        except ValueError:
+            return False, "Invalid Bet Number"
+
+        if self.bet_on <= 0 or self.bet_on > 28:
+            return False, "Invalid Bet Number"
+
+        return True, None
